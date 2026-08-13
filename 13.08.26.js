9fbaf6720845fcc59fa7756982077e4294315898
Aanyajaiswal <aanyajaiswal21@gmail.com>
2026-08-13 16:22:43 +0530

    array

---
+++ 13.08.26.js
@@ -0,0 +1,20 @@
+
+            let c=[1,2,3,4,5];
+            let l=c.push(6);
+            console.log(l);
+            let a="Aanya";
+            let b="Jaiswal";
+            a.concat(b);
+            console.log(a);
+            console.log(a.length);
+            c.shift;
+            console.log(c);
+            c.unshift(1);
+            console.log(c);
+            c.splice(2, 1);
+            console.log(c);
+            c.splice(2, 0, 3);
+            console.log(c);
+            c.splice(2, 0, 6, 7, 8);
+            console.log(c);
+        
